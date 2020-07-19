6d4d98d0441cc81abb7e2395865c531239e8c1cf
Madson Santana <madson.santana96@gmail.com>
2020-07-19 15:12:33 -0300

    app: barra de progrsso integrada com api

--- app/src/components/barra-progresso/index.js
+++ app/src/components/barra-progresso/index.js
@@ -5,62 +5,55 @@ import { Container } from "@material-ui/core";
 
 import { default as apiConnection } from '../../service/api-connection'
 
+const idColaborador = 2359
 
 export default (props) => {
-  const [mesReferencia, setMesReferencia] = React.useState(props.mesReferencia)
+  const mesReferencia = props.mesReferencia
+  const [horasBarra, setHorasBarra] = React.useState([])
 
-  const [horasBarra, setHorasBarra] = React.useState(0)
-  const [horasUteisMes, setHorasUteisMes] = React.useState(0)
-  const [horasUteisHoje, setHorasUteisHoje] = React.useState(0)
-  const [horasCadastradasAteHoje, setHorasCadastradasAteHoje] = React.useState(0)
-  const [BpPassado, setBpPassado] = React.useState();
-  const [BpPresente, setBpPresente] = React.useState();
-  const [BpFuturo, setBpFuturo] = React.useState();
-  const [BpExtra, setBpExtra] = React.useState();
+  const calculaValoresBarra = (res) => {
+    const uteisMes = res[0]
+    const uteisHoje = res[1]
+    const cadastradas = res[2]
 
-  const idColaborador = 2359
+    const cadastradaPorCento = 100 * cadastradas / uteisMes
+    const uteisHojePorCento = 100 * uteisHoje / uteisMes
 
-  React.useEffect(() => {
-    const {
-      uteisMes,
-      uteisHoje,
-      cadastradasAteHoje
-    } = horasBarra
+    if (cadastradaPorCento < uteisHojePorCento) {
+      setCadastradas(cadastradaPorCento)
+      setFaltaCadastrar(uteisHojePorCento - cadastradaPorCento)
+      setRestante(100 - uteisHojePorCento)
+    } else if (cadastradaPorCento > uteisHojePorCento) {
+      setCadastradas(cadastradaPorCento)
+      setRestante(100 - cadastradaPorCento)
 
-
-    const pcCadastrado = 100 * cadastradasAteHoje / uteisMes
-    const pcPrevistoHoje = (100 * uteisHoje / uteisMes) - pcCadastrado
-    const pcRestante = 100 - pcCadastrado - pcPrevistoHoje
-    
-    console.log(pcCadastrado, pcPrevistoHoje, pcRestante)
-    console.log(uteisMes, uteisHoje, cadastradasAteHoje)
-
-    setBpExtra(10)
-    setBpFuturo(pcRestante)
-    setBpPresente(pcPrevistoHoje)
-    setBpPassado(pcCadastrado)
-  }, [horasBarra])
+    }
+  }
 
   React.useEffect(() => {
     apiConnection.colaborador.GetDadosBarraProgresso(idColaborador, mesReferencia)
       .then(res => {
-
         if (res.length) {
           setHorasBarra(res)
+          calculaValoresBarra(res)
         } else {
           setHorasBarra([])
         }
       })
 
   }, [mesReferencia])
+  const [cadastradas, setCadastradas] = React.useState();
+  const [faltaCadastrar, setFaltaCadastrar] = React.useState();
+  const [restante, setRestante] = React.useState();
+  const [extra, setExtra] = React.useState();
 
   return (
     <Container>
       <ProgressBar className="barra-progresso">
-        <ProgressBar variant="success" now={BpPassado} key={1} />
-        <ProgressBar animated variant="success" now={BpPresente} key={2} />
-        <ProgressBar animated now={BpFuturo} key={3} />
-        <ProgressBar variant="warning" now={BpExtra} key={4} />
+        <ProgressBar variant="success" now={cadastradas} key={2} />
+        <ProgressBar animated variant="success" now={faltaCadastrar} key={1} />
+        <ProgressBar animated now={restante} key={3} />
+        {/* <ProgressBar variant="warning" now={extra} key={4} /> */}
       </ProgressBar>
     </Container>
   );
