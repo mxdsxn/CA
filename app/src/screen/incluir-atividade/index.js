b7b47a3e19e69a84837922ad66c98d21a2b96067
Madson Santana <madson.santana96@gmail.com>
2020-08-02 02:54:26 -0300

    app: logica tela de incluir aitivdade

--- app/src/screen/incluir-atividade/index.js
+++ app/src/screen/incluir-atividade/index.js
@@ -109,23 +109,27 @@ export default (props) => {
       listaProjetoDefault !== listasDefault.projetoDefault ?? setListaProjetoDefault(listasDefault.projetoDefault)
       listaCoordenador !== listasDefault.coordenador ?? setListaCoordenador(listasDefault.coordenador)
 
-      if (listaProjeto.find(x => x.IdProjeto === projetoSelecionado).IdProjetoTipo !== 4)
+      if (listaProjeto.find(x => x.IdProjeto === projetoSelecionado).IdProjetoTipo !== 4) {
         apiConnection.projetoMetodologiaFase.GetProjetoFaseByIdProjeto(projetoSelecionado)
           .then(res =>
             res ?
               setListaProjetoFase([].concat(listasDefault.projetoFase, res)) :
               setListaProjetoFase(listasDefault.projetoFase)
           )
-      setProjetoFaseSelecionado(0)
-
-      if (listaProjeto.find(x => x.IdProjeto === projetoSelecionado).IdProjetoTipo === 4)
+        setProjetoFaseSelecionado(0)
+        setListaCategoriaAtividade(listasDefault.categoriaAtividade)
+      }
+      
+      if (listaProjeto.find(x => x.IdProjeto === projetoSelecionado).IdProjetoTipo === 4) {
         apiConnection.projetoCategoriaAtividade.GetProjetoCategoriaAtividadeByIdProjeto(projetoSelecionado)
           .then(res =>
             res ?
               setListaCategoriaAtividade([].concat(listasDefault.categoriaAtividade, res)) :
               setListaCategoriaAtividade(listasDefault.categoriaAtividade)
           )
-      setCategoriaAtividadeSelecionado(0)
+        setCategoriaAtividadeSelecionado(0)
+        setListaProjetoFase(listasDefault.projetoFase)
+      }
 
     } else
       // se projeto é default (-1), carrega projetos default e coordenadores 
@@ -149,6 +153,8 @@ export default (props) => {
           )
         setCoordenadorSelecionado(0)
 
+        setListaCategoriaAtividade(listasDefault.categoriaAtividade)
+        setListaProjetoFase(listasDefault.projetoFase)
       } else if (projetoSelecionado === 0) {
         setListaProjetoFase(listasDefault.projetoFase)
         setListaCategoriaAtividade(listasDefault.categoriaAtividade)
