6ef56e9048f3a3372d8beb157bfa1ae0ebb1b1f5
Madson Santana <madson.santana96@gmail.com>
2020-08-06 00:28:13 -0300

    app: api url base

--- app/src/service/api-baseUrl.js
+++ app/src/service/api-baseUrl.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-/* baseUrl dev */
 const baseUrl = axios.create({
-  baseURL: "http://192.168.100.19:1111/",
+  baseURL: "http://localhost:1111/",
 });
-
-/* baseUrl prod */
-// const api = axios.create({
-//   baseURL: "http://192.168.100.19:1111/",
-// });
 
 export default baseUrl;
