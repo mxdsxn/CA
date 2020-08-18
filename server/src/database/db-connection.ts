7e3a560d2068995d57ba29598306efe4266ce846
Madson Santana <madson.santana96@gmail.com>
2020-08-18 19:45:04 -0300

    api: instalacao dotenv

--- server/src/database/db-connection.ts
+++ server/src/database/db-connection.ts
@@ -1,13 +1,22 @@
 import knex from 'knex'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const connection = knex({
   client: 'mssql',
   connection: {
-    server: 'VMDEV006.dom_qp.com.br\\DEV',
-    user: 'sistemas-internos',
-    password: 'u@m8w*d6',
-    database: 'Inovacred'
+    server: process.env.SERVER,
+    user: process.env.SERVER_USER,
+    password: process.env.SERVER_PASSWORD,
+    database: process.env.DATABASE
   }
 })
+
+console.log(process.env.SERVER)
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+console.log(process.env.DATABASE)
+console.log(process.env.PORT)
 
 export default connection
