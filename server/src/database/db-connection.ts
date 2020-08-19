d55a13420b5ce5ed4802171661a5d66e3ec5acc8
Madson Santana <madson.santana96@gmail.com>
2020-08-19 16:39:23 -0300

    app: configurando .env

--- server/src/database/db-connection.ts
+++ server/src/database/db-connection.ts
@@ -1,22 +1,13 @@
 import knex from 'knex'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 const connection = knex({
   client: 'mssql',
   connection: {
     server: process.env.SERVER,
-    user: process.env.SERVER_USER,
-    password: process.env.SERVER_PASSWORD,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DATABASE
   }
 })
-
-console.log(process.env.SERVER)
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
-console.log(process.env.DATABASE)
-console.log(process.env.PORT)
 
 export default connection
