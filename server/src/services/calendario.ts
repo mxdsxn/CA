9b51d7c880576a809cdd8e9ba94bd340fe7c1f89
Madson Santana <madson.santana96@gmail.com>
2020-09-07 00:48:09 -0300

    feat(moment): rotas calendario

--- server/src/services/calendario.ts
+++ server/src/services/calendario.ts
@@ -3,21 +3,22 @@ import { DiaModel } from '@models'
 import { CalendarioEntity } from '@entities'
 import libUtc from '@libUtc'
 import { CalendarioRepository as Repo } from '@repositories'
+import moment, { Moment } from 'moment'
 
 /* retorna lista de feriados no mes */
-const feriadosByIdColaboradorMes = async (idColaborador: number, mesReferencia: Date) => {
+const feriadosByIdColaboradorMes = async (idColaborador: number, mesReferencia: Moment) => {
   const listaFeriadosMes = await Repo.feriadosByIdColaboradorMes(idColaborador, mesReferencia)
 
   return listaFeriadosMes
 }
 
-const ListaFeriadoFinalSemanaByMes = async (idColaborador: number, mesReferencia: Date) => {
-  const listaFeriados = await Repo.feriadosByIdColaboradorMes(idColaborador, libUtc.getMonth(mesReferencia))
+const ListaFeriadoFinalSemanaByMes = async (idColaborador: number, mesReferencia: Moment) => {
+  const listaFeriados = await Repo.feriadosByIdColaboradorMes(idColaborador, mesReferencia)
     .then((suc) => {
       const listaFeriadoDia = suc.map(feriado => {
         const result: DiaModel = {
           Descricao: feriado.Descricao,
-          Dia: feriado.Dia
+          Dia: moment(feriado.Dia)
         }
         return result
       })
@@ -25,18 +26,18 @@ const ListaFeriadoFinalSemanaByMes = async (idColaborador: number, mesReferencia
     })
 
   const mesReferenciaInicio = mesReferencia
-  const mesReferenciaFim = libUtc.getEndMonth(mesReferenciaInicio)
+  const mesReferenciaFim = mesReferencia.endOf('month')
 
   var listaFinalSemana: DiaModel[] = []
 
-  for (let dia = mesReferenciaInicio; dia < mesReferenciaFim; dia = libUtc.addDay(dia)) {
-    if (dia.getDay() === 5) {
+  for (let dia = mesReferenciaInicio; dia.isSameOrBefore(mesReferenciaFim); dia.add(1, 'day')) {
+    if (dia.date() === 5) {
       const result: DiaModel = {
         Descricao: 'Sabado',
         Dia: dia
       }
       listaFinalSemana.push(result)
-    } else if (dia.getDay() === 6) {
+    } else if (dia.date() === 6) {
       const result: DiaModel = {
         Descricao: 'Domingo',
         Dia: dia
@@ -45,9 +46,9 @@ const ListaFeriadoFinalSemanaByMes = async (idColaborador: number, mesReferencia
     }
   }
 
-  const listaFeriadoDiaDeSemana = listaFeriados.filter(feriado => feriado.Dia.getDay() !== 6 && feriado.Dia.getDay() !== 5)
+  const listaFeriadoDiaDeSemana = listaFeriados.filter(feriado => feriado.Dia.date() !== 6 && feriado.Dia.date() !== 5)
 
-  const listaFeriadoFinalSemana = listaFeriadoDiaDeSemana.concat(listaFinalSemana).sort((x, y) => x.Dia.getTime() - y.Dia.getTime())
+  const listaFeriadoFinalSemana = listaFeriadoDiaDeSemana.concat(listaFinalSemana).sort((x, y) => x.Dia.millisecond() - y.Dia.millisecond())
   return listaFeriadoFinalSemana
 }
 
